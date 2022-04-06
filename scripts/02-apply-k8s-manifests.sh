2f8b451f078aabd6880ee6300e9a524d2862bd8c
Lucas Duarte <lucasdu@amazon.com>
2022-04-06 12:20:33 -0300

    feat: Created k8s scprit to apply manifests

---
+++ scripts/02-apply-k8s-manifests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Changing directory to build inside the application folders
+cd ..
+
+apply_manifests() {
+    service_folder=$1
+    cd sample-apps/$service_folder/
+    echo $PWD # Check Directory
+    kubectl apply -f kubernetes/
+    cd ../..
+}
+
+apply_manifests '00-fluentBit'
+apply_manifests '01-data-preper'
+apply_manifests '02-otel-collector'
+apply_manifests '03-mysql'
+apply_manifests '04-analytics-service'
+apply_manifests '05-databaseService'
+apply_manifests '06-orderService'
+apply_manifests '07-inventoryService'
+apply_manifests '08-paymentService'
+apply_manifests '09-recommendationService'
+apply_manifests '10-authenticationService'
+apply_manifests '11-client'
