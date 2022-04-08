c3b445d2b50736b45e80d6ee215fc613a3fb628c
Rafael Gumiero <rafael.gumiero@gmail.com>
2022-04-08 16:12:32 -0300

    Changed the setup script with the necessary tools

--- 00-setup.sh
+++ 00-setup.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-echo `date`
-echo `hostname`
-echo "all my positional parameters $@" >> positional_params
 
-curl -s -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose >> /dev/null
-chmod a+x /usr/bin/docker-compose
+# Install jq
+sudo yum -y -q install jq
 
-openssl genrsa -out mydomain.key 2048
-openssl req -new -key mydomain.key -passin pass:password -subj /C=US/ST=Kansas/L=Lawrence/O=OrgName/OU=OuName/CN=CnName/emailAddress=aos@mydomain.com -out mydomain.csr
-openssl x509 -req -days 365 -in mydomain.csr -signkey mydomain.key -out mydomain.crt
-cat mydomain.key mydomain.crt >> /etc/ssl/certs/mydomain.pem
+# Update awscli
+pip install --user --upgrade awscli
 
-yum -y install docker
-service docker start
+# Install awscli v2
+curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" 
+unzip -o awscli-exe-linux-x86_64.zip
+sudo ./aws/install
+rm awscli-exe-linux-x86_64.zip
 
-yum -y install haproxy
-yum -y install git
+# Install bash-completion
+sudo yum -y install jq gettext bash-completion moreutils
 
-git clone https://github.com/muthup/traceanalytics.git
+# Install kubectl 1.21.2
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+echo "source <(kubectl completion bash)" >> ~/.bashrc
 
-cd traceanalytics/ant403
+# Configure AWS CLI
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
 
-grep -RIl 'OSS_DOMAIN' | xargs sed -i -e "s/OSS_DOMAIN/$1/g" -e "s/OSSDOMAIN_USERNAME/aosadmin/g" -e "s/OSSDOMAIN_PASSWORD/Passw0rd1!/g"
-/bin/cp haproxy.cfg /etc/haproxy/
-service haproxy start
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+aws configure get default.region
 
-/usr/bin/docker-compose up -d
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
 
+# Create a CMK for the EKS cluster to use when encrypting your Kubernetes secrets
+aws kms create-alias --alias-name alias/observability-workshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
+export MASTER_ARN=$(aws kms describe-key --key-id alias/observability-workshop --query KeyMetadata.Arn --output text)
+echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 
+# Download lab repository
+git clone https://github.com/rafael-gumiero/observability-aos
