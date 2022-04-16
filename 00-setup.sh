1b90efd6b5328ef801b3889ea9e4f3f1a955125d
Rafael Gumiero <rafael.gumiero@gmail.com>
2022-04-16 17:20:09 -0300

    Update network variables (VPC, Priv/Pub-subnets)

--- 00-setup.sh
+++ 00-setup.sh
@@ -30,6 +30,23 @@ echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
 
+# Configure network variables (VPC, Priv/Pub-subnets)
+export MyVPC=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='MyVPC'].OutputValue" --output text)
+export PrivateSubnet1=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1'].OutputValue" --output text)
+export PrivateSubnet2=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2'].OutputValue" --output text)
+export PrivateSubnet3=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet3'].OutputValue" --output text)
+export PublicSubnet1=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='PublicSubnet1'].OutputValue" --output text)
+export PublicSubnet2=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='PublicSubnet2'].OutputValue" --output text)
+export PublicSubnet3=$(aws cloudformation describe-stacks --stack-name Observability-AOS --query "Stacks[0].Outputs[?OutputKey=='PublicSubnet3'].OutputValue" --output text)
+
+echo "export MyVPC=${MyVPC}" | tee -a ~/.bash_profile
+echo "export PrivateSubnet1=${PrivateSubnet1}" | tee -a ~/.bash_profile
+echo "export PrivateSubnet2=${PrivateSubnet2}" | tee -a ~/.bash_profile
+echo "export PrivateSubnet3=${PrivateSubnet3}" | tee -a ~/.bash_profile
+echo "export PublicSubnet1=${PublicSubnet1}" | tee -a ~/.bash_profile
+echo "export PublicSubnet2=${PublicSubnet2}" | tee -a ~/.bash_profile
+echo "export PublicSubnet3=${PublicSubnet3}" | tee -a ~/.bash_profile
+
 # Create a CMK for the EKS cluster to use when encrypting your Kubernetes secrets
 aws kms create-alias --alias-name alias/observability-workshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/observability-workshop --query KeyMetadata.Arn --output text)
